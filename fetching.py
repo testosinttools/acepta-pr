a6837cf2f70f2518d2a69fa7dd4c8aa061a44b96
testosinttools@gmail.com <testosinttools@gmail.com>
2025-03-29 18:23:23 -0300

    a

--- fetching.py
+++ fetching.py
@@ -1,17 +1,22 @@
 import asyncio
 
-
 """
 Programa que intenta hacer cosas cool
 """
 
 
 async def task1():
+    """
+    Esta tarea trae desde el servidor un archivo
+    """
     await asyncio.sleep(2)
     return 1
 
 
 async def task2():
+    """
+    Esta tarea tb
+    """
     await asyncio.sleep(2)
     return 1
 
